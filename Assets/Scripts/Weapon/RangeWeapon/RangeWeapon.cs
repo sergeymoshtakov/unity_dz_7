03faa38907bffa220604bb0c426969ca5f540bfe
Serhii Moshtakov <serhii.moshtakov@students.fhnw.ch>
2024-11-26 20:56:06 +0100

    first commit

---
+++ Assets/Scripts/Weapon/RangeWeapon/RangeWeapon.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace Weapon.RangeWeapon
+{
+    public abstract class RangeWeapon : Weapon
+    {
+        [SerializeField] private Camera _camera;
+        [SerializeField] private Transform _weaponPoint;
+        [SerializeField] private float _range;
+        
+        protected override void Attack()
+        {
+            if (PlayerInput.AttackInput)
+            {
+                var ray = _camera.ViewportPointToRay(_weaponPoint.position);
+
+                if (Physics.Raycast(ray, out var hit, _range))
+                {
+                    if (hit.transform.TryGetComponent(out Enemy.Enemy enemy))
+                    {
+                        print(enemy.name);
+                    }
+                }
+            }
+        }
+
+        private void OnDrawGizmos()
+        {
+            Debug.DrawRay(_weaponPoint.position, _weaponPoint.forward * _range, Color.red);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# unity_dz_7
+
+![result](images/result-1.png)
+
+![result](images/result-2.png)
